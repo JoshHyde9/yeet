b0a495dd90dec9de256821b997fdc675e9eb6a83
JoshHyde9 <joshhyde546@gmail.com>
2024-08-09 13:40:52 +1000

    feat: added env vars

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,6 +7,9 @@ use serde::Serialize;
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
 
+use dotenv::dotenv;
+use std::env;
+
 #[derive(Serialize)]
 struct Response {
     message: String,
@@ -22,24 +25,24 @@ async fn hello_world() -> Json<Response> {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let host = env::var("HOST").expect("Error: host env not found");
+    let port = env::var("PORT").expect("Error: port env not found");
+
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap());
+        .allow_origin(
+            format!("http://{host}:{}", 3000)
+                .parse::<HeaderValue>()
+                .unwrap(),
+        );
 
     let app = Router::new().route("/", get(hello_world).layer(cors));
 
-    let address = "localhost";
-    let port = 5000;
+    let listener = TcpListener::bind(format!("{host}:{port}")).await.unwrap();
 
-    let listener = TcpListener::bind(format!("{}:{}", address, port.to_string()))
-        .await
-        .unwrap();
-
-    println!(
-        "Server listening at http://{}:{}",
-        address,
-        port.to_string()
-    );
+    println!("Server listening at http://{host}:{port}");
 
     axum::serve(listener, app).await.unwrap();
 }
