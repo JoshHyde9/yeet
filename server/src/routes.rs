2e8dd0b504358150bb2639592c6ab6cd152cda77
JoshHyde9 <joshhyde546@gmail.com>
2024-08-09 22:11:40 +1000

    fix: removed testing garbage

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -5,16 +5,10 @@ use axum::{
     routing::get,
     Extension, Json, Router,
 };
-use serde::Serialize;
 
 use crate::prisma::PrismaClient;
 
 pub type Database = Extension<Arc<PrismaClient>>;
-
-#[derive(Serialize)]
-pub struct Data {
-    message: String,
-}
 
 async fn hello_world(db: Database) -> Response {
     let users = db.user().find_many(vec![]).exec().await.unwrap();
