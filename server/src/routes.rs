d8ffea8f071bf32fe40b039656dfa38d1e71b564
JoshHyde9 <joshhyde546@gmail.com>
2024-08-10 11:20:43 +1000

    feat: added user creation

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,10 +1,16 @@
 use std::{sync::Arc, vec};
 
+use argon2::{password_hash::rand_core::OsRng, Argon2};
 use axum::{
+    body::Body,
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, post},
     Extension, Json, Router,
 };
+
+use argon2::{password_hash::SaltString, PasswordHasher};
+
+use serde::{Deserialize, Serialize};
 
 use crate::prisma::PrismaClient;
 
@@ -16,6 +22,39 @@ async fn hello_world(db: Database) -> Response {
     Json(users).into_response()
 }
 
+#[derive(Deserialize, Serialize)]
+struct CreateUser {
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+}
+
+async fn register(db: Database, Json(body): Json<CreateUser>) -> Response<Body> {
+    let password = body.password;
+
+    let argon2 = Argon2::default();
+    let salt: SaltString = SaltString::generate(&mut OsRng);
+
+    let hashed_password = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+
+    let user = db
+        .user()
+        .create(
+            body.first_name,
+            body.last_name,
+            body.email,
+            hashed_password.to_string(),
+            vec![],
+        )
+        .exec()
+        .await;
+
+    Json(user.ok()).into_response()
+}
+
 pub fn create_route() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/user/register", post(register))
 }
